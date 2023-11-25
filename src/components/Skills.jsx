68f8f32fafd58d8682a84812972a7f699f035a55
Emerson Marques Pedro <emerson.pedro@alumni.usp.br>
2023-11-25 20:15:18 -0300

    Add files via upload

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,156 +12,172 @@ function Skills() {
           <div className="col-md-6">
             <h3>Tecnologias que uso no meu dia-a-dia:</h3>
             <p>
-              <img
-                alt="HTML5"
-                title="HTML5"
-                src="https://camo.githubusercontent.com/d84a97169f367252b038bd49673b72ddaacd1aa629fab5ca98c617576f4df0ab/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d48544d4c352d4533344632363f6c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "100%" }}
-                  aria-valuenow="100"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  100%
+              <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5">
+                <img
+                  alt="HTML5"
+                  title="HTML5"
+                  src="https://camo.githubusercontent.com/d84a97169f367252b038bd49673b72ddaacd1aa629fab5ca98c617576f4df0ab/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d48544d4c352d4533344632363f6c6f676f3d68746d6c35266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "100%" }}
+                    aria-valuenow="100"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    100%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
             <p>
-              <img
-                alt="CSS3"
-                title="CSS3"
-                src="https://camo.githubusercontent.com/885c3621e019c7ee86f21e0ae296c234962e666b6659350e7ba29d94cd176af1/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4353532d3135373242363f6c6f676f3d63737333266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "95%" }}
-                  aria-valuenow="95"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  95%
+              <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+                <img
+                  alt="CSS3"
+                  title="CSS3"
+                  src="https://camo.githubusercontent.com/885c3621e019c7ee86f21e0ae296c234962e666b6659350e7ba29d94cd176af1/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4353532d3135373242363f6c6f676f3d63737333266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "95%" }}
+                    aria-valuenow="95"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    95%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
             <p>
-              <img
-                alt="JavaScript"
-                title="JavaScript"
-                src="https://camo.githubusercontent.com/7db489a0693acdc3a7251ca96e10488ac19269a678fbfc8be4d50035de849d0b/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4a6176615363726970742d4637444631453f6c6f676f3d6a617661736372697074266c6f676f436f6c6f723d626c61636b267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "90%" }}
-                  aria-valuenow="90"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  90%
+              <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">
+                <img
+                  alt="JavaScript"
+                  title="JavaScript"
+                  src="https://camo.githubusercontent.com/7db489a0693acdc3a7251ca96e10488ac19269a678fbfc8be4d50035de849d0b/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4a6176615363726970742d4637444631453f6c6f676f3d6a617661736372697074266c6f676f436f6c6f723d626c61636b267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "90%" }}
+                    aria-valuenow="90"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    90%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
             <p>
-              <img
-                alt="React"
-                title="React"
-                src="https://camo.githubusercontent.com/cd1477459fb25aeab339332013164fd2d43de367bce96df822079bb503475e0c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d52656163742d3631444146423f6c6f676f3d7265616374266c6f676f436f6c6f723d626c61636b267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "80%" }}
-                  aria-valuenow="80"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  80%
+              <a href="https://reactjs.org/">
+                <img
+                  alt="React"
+                  title="React"
+                  src="https://camo.githubusercontent.com/cd1477459fb25aeab339332013164fd2d43de367bce96df822079bb503475e0c/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d52656163742d3631444146423f6c6f676f3d7265616374266c6f676f436f6c6f723d626c61636b267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "80%" }}
+                    aria-valuenow="80"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    80%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
             <p>
-              <img
-                alt="Node.js"
-                title="Node.js"
-                src="https://camo.githubusercontent.com/83350497b4972a45c730b7971a3adc783857596f95f9248c58541e4ad2bc625a/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4e6f64652e6a732d3333393933333f6c6f676f3d6e6f64652e6a73266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "75%" }}
-                  aria-valuenow="75"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  75%
+              <a href="https://nodejs.org/">
+                <img
+                  alt="Node.js"
+                  title="Node.js"
+                  src="https://camo.githubusercontent.com/83350497b4972a45c730b7971a3adc783857596f95f9248c58541e4ad2bc625a/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d4e6f64652e6a732d3333393933333f6c6f676f3d6e6f64652e6a73266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "75%" }}
+                    aria-valuenow="75"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    75%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
             <p>
-              <img
-                alt="Python"
-                title="Python"
-                src="https://camo.githubusercontent.com/80961983b84d0feb040afb31b1516b52b88a22717e671b170c32e2dac5e8efee/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d507974686f6e2d3337373641423f6c6f676f3d707974686f6e266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "85%" }}
-                  aria-valuenow="85"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  85%
+              <a href="https://www.python.org/">
+                <img
+                  alt="Python"
+                  title="Python"
+                  src="https://camo.githubusercontent.com/80961983b84d0feb040afb31b1516b52b88a22717e671b170c32e2dac5e8efee/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d507974686f6e2d3337373641423f6c6f676f3d707974686f6e266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "85%" }}
+                    aria-valuenow="85"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    85%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
             <p>
-              <img
-                alt="Visual Studio Code Editor"
-                title="Visual Studio Code Editor"
-                src="https://camo.githubusercontent.com/09d5b2acec49e07670a361469e10b41dd2ca9383bab6c5457e30f54a97f3691e/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f56697375616c5f53747564696f5f436f64652d3030374143433f6c6f676f3d76697375616c2d73747564696f2d636f6465266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "90%" }}
-                  aria-valuenow="90"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  90%
+              <a href="https://code.visualstudio.com">
+                <img
+                  alt="Visual Studio Code Editor"
+                  title="Visual Studio Code Editor"
+                  src="https://camo.githubusercontent.com/09d5b2acec49e07670a361469e10b41dd2ca9383bab6c5457e30f54a97f3691e/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f56697375616c5f53747564696f5f436f64652d3030374143433f6c6f676f3d76697375616c2d73747564696f2d636f6465266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "90%" }}
+                    aria-valuenow="90"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    90%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
             <p>
-              <img
-                alt="WordPress"
-                title="WordPress"
-                src="https://camo.githubusercontent.com/c27e789f3ddaa2dd0dd49edcab7be0742bcd51cb24ec4be1a5b4ca37be0e387a/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d576f726450726573732d3231373539423f6c6f676f3d776f72647072657373266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
-              />
-              <div className="progress">
-                <div
-                  className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                  role="progressbar"
-                  style={{ width: "95%" }}
-                  aria-valuenow="95"
-                  aria-valuemin="0"
-                  aria-valuemax="100"
-                >
-                  95%
+              <a href="https://wordpress.org/">
+                <img
+                  alt="WordPress"
+                  title="WordPress"
+                  src="https://camo.githubusercontent.com/c27e789f3ddaa2dd0dd49edcab7be0742bcd51cb24ec4be1a5b4ca37be0e387a/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2d576f726450726573732d3231373539423f6c6f676f3d776f72647072657373266c6f676f436f6c6f723d7768697465267374796c653d666c6174"
+                />
+                <div className="progress">
+                  <div
+                    className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                    role="progressbar"
+                    style={{ width: "95%" }}
+                    aria-valuenow="95"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  >
+                    95%
+                  </div>
                 </div>
-              </div>
+              </a>
             </p>
           </div>
           <div className="col-md-6">
